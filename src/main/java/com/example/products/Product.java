3db994dc8ef99fd673284b405b472384c07a7b30
Matt Fellows <matt.fellows@onegeek.com.au>
2021-08-30 18:02:45 +1000

    feat: show how to serialise null values into contract

--- src/main/java/com/example/products/Product.java
+++ src/main/java/com/example/products/Product.java
@@ -3,15 +3,19 @@ package com.example.products;
 import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Product {
-  @JsonFormat( shape = JsonFormat.Shape.STRING)
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
   private String id;
   private String name;
   private String type;
 
-  Product() {}
+  Product() {
+  }
 
   Product(String id, String name, String type) {
     this.id = id;
--- src/test/java/com/example/products/ProductsPactTest.java
+++ src/test/java/com/example/products/ProductsPactTest.java
@@ -17,53 +17,42 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(providerName = "pactflow-example-provider-springboot")
 public class ProductsPactTest {
 
-  @Pact(consumer="pactflow-example-consumer-java-junit")
+  // Arrange
+  @Pact(consumer = "pactflow-example-consumer-java-junit")
   public RequestResponsePact getProduct(PactDslWithProvider builder) {
 
     PactDslJsonBody body = new PactDslJsonBody();
     body.stringType("name", "product name");
     body.stringType("type", "product series");
     body.stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850");
+    body.nullValue("foo");
+    body.object("amount").nullValue("baz").nullValue("bat").close();
 
-      return builder
-        .given("a product with ID 10 exists")
-        .uponReceiving("a request to get a product")
-          .path("/product/10")
-          .method("GET")
-        .willRespondWith()
-          .status(200)
-          .body(body)
-        .toPact();
-    }
+    return builder.given("a product with ID 10 exists").uponReceiving("a request to get a product").path("/product/10")
+        .method("GET").willRespondWith().status(200).body(body).toPact();
+  }
 
   @PactTestFor(pactMethod = "getProduct")
   @Test
   public void testGetProduct(MockServer mockServer) throws IOException {
+    // Act
     Product product = new ProductClient().setUrl(mockServer.getUrl()).getProduct("10");
 
+    // Assert
     assertThat(product.getId(), is("5cc989d0-d800-434c-b4bb-b1268499e850"));
   }
 
-  @Pact(consumer="pactflow-example-consumer-java-junit")
+  @Pact(consumer = "pactflow-example-consumer-java-junit")
   public RequestResponsePact getProducts(PactDslWithProvider builder) {
-      return builder
-          .given("a product with ID 10 exists")
-          .uponReceiving("a request to get all products")
-              .path("/products")
-              .method("GET")
-          .willRespondWith()
-              .status(200)
-              .body(PactDslJsonArray.arrayEachLike()
-                .stringType("name", "product name")
-                .stringType("type", "product series")
-                .stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850")
-                .closeObject())
-          .toPact();
+    return builder.given("a product with ID 10 exists").uponReceiving("a request to get all products").path("/products")
+        .method("GET").willRespondWith().status(200)
+        .body(PactDslJsonArray.arrayEachLike().stringType("name", "product name").stringType("type", "product series")
+            .stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850").closeObject())
+        .toPact();
   }
 
   @PactTestFor(pactMethod = "getProducts")
