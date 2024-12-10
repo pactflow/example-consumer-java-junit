05d4445d9748a8dae879d36e0f3e8e2697ddaa04
Yousaf Nabi <yousaf@pactflow.io>
2024-12-10 16:13:45 +0000

    chore(test): change a product id

--- src/test/java/com/example/products/ProductsPactTest.java
+++ src/test/java/com/example/products/ProductsPactTest.java
@@ -28,7 +28,7 @@ public class ProductsPactTest {
     PactDslJsonBody body = new PactDslJsonBody();
     body.stringType("name", "product name");
     body.stringType("type", "product series");
-    body.stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850");
+    body.stringType("id", "5cc989d1-d800-434c-b4bb-b1268499e850");
 
       return builder
         .given("a product with ID 10 exists")
@@ -46,7 +46,7 @@ public class ProductsPactTest {
   public void testGetProduct(MockServer mockServer) throws IOException {
     Product product = new ProductClient().setUrl(mockServer.getUrl()).getProduct("10");
 
-    assertThat(product.getId(), is("5cc989d0-d800-434c-b4bb-b1268499e850"));
+    assertThat(product.getId(), is("5cc989d1-d800-434c-b4bb-b1268499e850"));
   }
 
   @Pact(consumer="pactflow-example-consumer-java-junit")
@@ -61,7 +61,7 @@ public class ProductsPactTest {
               .body(PactDslJsonArray.arrayEachLike()
                 .stringType("name", "product name")
                 .stringType("type", "product series")
-                .stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850")
+                .stringType("id", "5cc989d1-d800-434c-b4bb-b1268499e850")
                 .closeObject())
           .toPact();
   }
@@ -71,6 +71,6 @@ public class ProductsPactTest {
   public void testGetProducts(MockServer mockServer) throws IOException {
     List<Product> products = new ProductClient().setUrl(mockServer.getUrl()).getProducts();
 
-    assertThat(products.get(0).getId(), is("5cc989d0-d800-434c-b4bb-b1268499e850"));
+    assertThat(products.get(0).getId(), is("5cc989d1-d800-434c-b4bb-b1268499e850"));
   }
 }
